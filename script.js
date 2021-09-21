063e7a5636a5af36d998936059be32b06d4790f0
myth <mmaloney31@yahoo.com>
2021-09-21 18:18:37 -0400

    Updated Create Grid

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-let container = document.getElementById("container");
-let button = document.querySelector("button")
-button.addEventListener("click" , resetAndCreateGrid)
+let container = document.getElementById("#container")
+let createButton = document.querySelector(".create")
+let resetButton = document.querySelector(".reset")
+let input = document.querySelector("input")
+let color = changeColor() 
 
-function createGrid(rows , columns) {
+
+function createGrid() {
+    columnsAndRows = input.value
+    container.style.gridTemplateColumns = `repeat(${columnsAndRows}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${columnsAndRows}, 1fr)`
+    container.style.gridGap = "1px"
+    
     for (let i = 0; i < (rows * columns); i++) {
         let div = document.createElement("div")
         div.addEventListener("mouseover" , e => e.target.classList.add("hover"))
         div.classList.add("square")
+        div.style.backgroundColor = "black"
         container.appendChild(div)
     }
 }
-createGrid(16 , 16);
 
-function resetAndCreateGrid(rows , columns) {
-    let resetDiv = document.querySelectorAll(".square")
-    resetDiv.forEach(square => square.classList.remove("hover"))
-    // prompt user for 2 numbers for rows and columns
-    // create grid from those 2 numbers
-    // set max limit for each user input up to 100
+function resetGrid() {
+   
+    
+}
+
+function changeColor () {
+
 
 }
 
+createButton.addEventListener("click" , createGrid)
+resetButton.addEventListener("click" , resetGrid)
 
+// if (userInput <= 100) {
+//     return true
+// }
+// else if (userInput <= 0) {
+//     return false
+// }
+// else {
+//     return false
+// }
 
+// for (let i = 0; i < (num * num); i++) {
+//     let div = document.createElement("div")
+//     div.addEventListener("mouseover" , e => e.target.classList.add("hover"))
+//     div.classList.add("square")
+//     container.appendChild(div)
+// }
+// let clearDiv = document.querySelectorAll(".square")
+//     clearDiv.forEach(square => square.classList.remove("hover"))
+//     let userInput = prompt("Enter A Number To Create A New Grid" , " " )
