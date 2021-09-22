32ebd2f4af663fe835a7de1d16b744a615db3581
myth <mmaloney31@yahoo.com>
2021-09-21 20:13:01 -0400

    Updated JS functions

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-let container = document.getElementById("#container")
+let container = document.querySelector("#container")
 let createButton = document.querySelector(".create")
 let resetButton = document.querySelector(".reset")
 let input = document.querySelector("input")
-let color = changeColor() 
+let color = changeColor()
+
+window.onload = createGrid
 
 
 function createGrid() {
+    if (container.firstElementChild) {
+        while (container.firstElementChild) {
+            container.removeChild(container.firstElementChild)
+        }
+    }
     columnsAndRows = input.value
     container.style.gridTemplateColumns = `repeat(${columnsAndRows}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${columnsAndRows}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${columnsAndRows})`
     container.style.gridGap = "1px"
-    
-    for (let i = 0; i < (rows * columns); i++) {
+
+    for (let i = 0; i < (columnsAndRows * columnsAndRows); i++) {
         let div = document.createElement("div")
-        div.addEventListener("mouseover" , e => e.target.classList.add("hover"))
         div.classList.add("square")
-        div.style.backgroundColor = "black"
+        div.style.backgroundColor = "white"
+        div.addEventListener("mouseover", mouseOver)
         container.appendChild(div)
     }
+
 }
 
 function resetGrid() {
-   
-    
+    let squares = document.querySelectorAll(".square")
+    squares.forEach(square => square.style.backgroundColor = "white")
+    color = changeColor()
 }
 
-function changeColor () {
-
-
+function mouseOver(e) {
+    e.target.style.backgroundColor = color
 }
 
-createButton.addEventListener("click" , createGrid)
-resetButton.addEventListener("click" , resetGrid)
+function changeColor() {
+    let colorOne = Math.floor(Math.random() * 256);
+    let colorTwo = Math.floor(Math.random() * 256);
+    let colorThree = Math.floor(Math.random() * 256);
+    let randomColor = `rgb(${colorOne}, ${colorTwo}, ${colorThree})`
+    return randomColor
+}
 
-// if (userInput <= 100) {
-//     return true
-// }
-// else if (userInput <= 0) {
-//     return false
-// }
-// else {
-//     return false
-// }
-
-// for (let i = 0; i < (num * num); i++) {
-//     let div = document.createElement("div")
-//     div.addEventListener("mouseover" , e => e.target.classList.add("hover"))
-//     div.classList.add("square")
-//     container.appendChild(div)
-// }
-// let clearDiv = document.querySelectorAll(".square")
-//     clearDiv.forEach(square => square.classList.remove("hover"))
-//     let userInput = prompt("Enter A Number To Create A New Grid" , " " )
+createButton.addEventListener("click", createGrid)
+resetButton.addEventListener("click", resetGrid)
